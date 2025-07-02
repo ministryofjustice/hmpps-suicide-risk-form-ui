531b982ecf3a0b1418ed0b1082b335b9b63251cd
peter <peter.wilson@unilink.com>
2025-07-02 15:07:24 +0100

    initial_configuration

--- server/services/index.ts
+++ server/services/index.ts
@@ -3,12 +3,13 @@ import AuditService from './auditService'
 import ExampleService from './exampleService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, exampleApiClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, exampleApiClient, hmppsAuthClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     exampleService: new ExampleService(exampleApiClient),
+    hmppsAuthClient,
   }
 }
 
