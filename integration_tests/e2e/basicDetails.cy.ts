9bf0ceaeb0239d2927d0d0e938ae10ab44f0ca14
Joseph.Dundon <jdundon@unilink.com>
2025-07-25 14:25:57 +0100

    SRF-6 - test fix

--- integration_tests/e2e/basicDetails.cy.ts
+++ integration_tests/e2e/basicDetails.cy.ts
@@ -1,5 +1,4 @@
 context('Basic Details page', () => {
-
   it('can see readonly fields', () => {
     cy.visit('/basic-details/00000000-0000-0000-0000-000000000001')
     cy.get('#page-title').should('contain.text', 'Suicide Risk Form - Basic Details')
