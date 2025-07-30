be352c7463b11f97f9c8d0d9d23549ce36c1e37c
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2025-07-30 11:22:28 +0100

    SRF-6

--- server/routes/basicDetails.ts
+++ server/routes/basicDetails.ts
@@ -2,9 +2,18 @@ import { Router } from 'express'
 import { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
 import AuditService, { Page } from '../services/auditService'
 
-import SuicideRiskApiClient from '../data/suicideRiskApiClient'
-import NDeliusIntegrationApiClient from '../data/ndeliusIntegrationApiClient'
+import SuicideRiskApiClient, { SuicideRisk, SuicideRiskAddress } from '../data/suicideRiskApiClient'
+import NDeliusIntegrationApiClient, { BasicDetails, DeliusAddress } from '../data/ndeliusIntegrationApiClient'
+import {
+  calculateAge,
+  findDefaultAddressInAddressList,
+  formatTitleAndFullName,
+  handleIntegrationErrors,
+  toIsoDateFormat,
+  toSuicideRiskAddress,
+} from '../utils/utils'
 import CommonUtils from '../services/commonUtils'
+import { ErrorMessages } from '../data/uiModels'
 
 export default function basicDetailsRoutes(
   router: Router,
@@ -14,23 +23,147 @@ export default function basicDetailsRoutes(
 ): Router {
   const currentPage = 'basic-details'
 
-  router.get('/basic-details/:id', async (req, res, next) => {
+  router.get('/basic-details/:id', async (req, res) => {
     await auditService.logPageView(Page.BASIC_DETAILS, { who: res.locals.user.username, correlationId: req.id })
-
     const suicideRiskApiClient = new SuicideRiskApiClient(authenticationClient)
-    const suicideRisk = await suicideRiskApiClient.getSuicideRiskById(req.params.id as string, res.locals.user.username)
-
     const ndeliusIntegrationApiClient = new NDeliusIntegrationApiClient(authenticationClient)
-    const basicDetails = await ndeliusIntegrationApiClient.getBasicDetails(suicideRisk.crn, res.locals.user.username)
+
+    const suicideRiskId: string = req.params.id
+    let suicideRisk: SuicideRisk = null
+    let basicDetails: BasicDetails = null
+
+    try {
+      suicideRisk = await suicideRiskApiClient.getSuicideRiskById(suicideRiskId, res.locals.user.username)
+    } catch (error) {
+      const errorMessages: ErrorMessages = handleIntegrationErrors(
+        error?.data?.status,
+        error?.data?.userMessage,
+        'Suicide Risk',
+      )
+
+      // Navigate to the detailed error page on 400
+      if (error?.data?.status === 400) {
+        res.render(`pages/detailed-error`, { errorMessages })
+        return
+      }
+
+      // Navigate to the detailed error page on 404
+      if (error?.data?.status === 404) {
+        res.render(`pages/detailed-error`, { errorMessages })
+        return
+      }
+
+      const showEmbeddedError = true
+      res.render(`pages/basic-details`, { errorMessages, showEmbeddedError })
+      return
+    }
+
+    try {
+      basicDetails = await ndeliusIntegrationApiClient.getBasicDetails(suicideRisk.crn, res.locals.user.username)
+    } catch (error) {
+      const errorMessages: ErrorMessages = handleIntegrationErrors(
+        error.status,
+        error.data?.message,
+        'NDelius Integration',
+      )
+      // take the user to detailed error page for 400 type errors
+      if (error.status === 400) {
+        res.render(`pages/detailed-error`, { errorMessages })
+        return
+      }
+
+      // stay on the current page for 500 errors
+      if (error.status === 500) {
+        const showEmbeddedError = true
+        res.render(`pages/basic-details`, { errorMessages, showEmbeddedError })
+        return
+      }
+      res.render(`pages/detailed-error`, { errorMessages })
+      return
+    }
 
     if (await commonUtils.redirectRequired(suicideRisk, res)) return
 
+    const age = calculateAge(basicDetails.dateOfBirth)
+    const defaultAddress: DeliusAddress = findDefaultAddressInAddressList(basicDetails.addresses)
+    const titleAndFullName: string = formatTitleAndFullName(basicDetails.title, basicDetails.name)
+    const { prisonNumber } = basicDetails
+
     res.render('pages/basic-details', {
       suicideRisk,
+      suicideRiskId,
       basicDetails,
+      age,
+      defaultAddress,
+      titleAndFullName,
+      prisonNumber,
       currentPage,
     })
   })
 
+  router.post('/basic-details/:id', async (req, res) => {
+    const suicideRiskApiClient = new SuicideRiskApiClient(authenticationClient)
+    const ndeliusIntegrationApiClient = new NDeliusIntegrationApiClient(authenticationClient)
+
+    const suicideRiskId: string = req.params.id
+    let suicideRisk: SuicideRisk = null
+    let basicDetails: BasicDetails = null
+
+    try {
+      suicideRisk = await suicideRiskApiClient.getSuicideRiskById(suicideRiskId, res.locals.user.username)
+    } catch (error) {
+      const errorMessages: ErrorMessages = handleIntegrationErrors(error.status, error.data?.message, 'Suicide Risk')
+      const showEmbeddedError = true
+      res.render(`pages/basic-details`, { errorMessages, showEmbeddedError })
+      return
+    }
+
+    try {
+      basicDetails = await ndeliusIntegrationApiClient.getBasicDetails(suicideRisk.crn, res.locals.user.username)
+    } catch (error) {
+      const errorMessages: ErrorMessages = handleIntegrationErrors(
+        error.status,
+        error.data?.message,
+        'NDelius Integration',
+      )
+      // take the user to detailed error page for 400 type errors
+      if (error.status === 400) {
+        res.render(`pages/detailed-error`, { errorMessages })
+        return
+      }
+      // stay on the current page for 500 errors
+      if (error.status === 500) {
+        const showEmbeddedError = true
+        res.render(`pages/basic-details`, { errorMessages, showEmbeddedError })
+        return
+      }
+      res.render(`pages/detailed-error`, { errorMessages })
+      return
+    }
+
+    const dob = toIsoDateFormat(basicDetails.dateOfBirth)
+    const defaultAddress: DeliusAddress = findDefaultAddressInAddressList(basicDetails.addresses)
+    const suicideRiskDefaultAddress: SuicideRiskAddress = toSuicideRiskAddress(defaultAddress)
+    const titleAndFullName: string = formatTitleAndFullName(basicDetails.title, basicDetails.name)
+    const { prisonNumber } = basicDetails
+
+    suicideRisk.dateOfBirth = dob
+    suicideRisk.titleAndFullName = titleAndFullName
+    suicideRisk.postalAddress = suicideRiskDefaultAddress
+    suicideRisk.prisonNumber = prisonNumber
+    suicideRisk.basicDetailsSaved = true
+
+    await suicideRiskApiClient.updateSuicideRisk(req.params.id, suicideRisk, res.locals.user.username)
+
+    if (req.body.action === 'saveProgressAndClose') {
+      res.send(
+        `<p>You can now safely close this window</p><script nonce="${res.locals.cspNonce}">window.close()</script>`,
+      )
+    } else if (req.body.action === 'refreshFromNdelius') {
+      res.redirect(`/basic-details/${req.params.id}`)
+    } else {
+      res.redirect(`/information/${req.params.id}`)
+    }
+  })
   return router
 }
