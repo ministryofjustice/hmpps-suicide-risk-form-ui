bdd1af7344c015310837e4850af5f9656d5542d7
Joseph.Dundon <jdundon@unilink.com>
2025-07-30 09:36:20 +0100

    SRF-6 - Remove duplicate test

--- server/utils/utils.test.ts
+++ server/utils/utils.test.ts
@@ -87,19 +87,6 @@ describe('initialise error messages', () => {
   )
 })
 
-describe('initialise name', () => {
-  it.each([
-    [null, null, null],
-    ['Empty string', '', null],
-    ['One word', 'robert', 'r. robert'],
-    ['Two words', 'Robert James', 'R. James'],
-    ['Three words', 'Robert James Smith', 'R. Smith'],
-    ['Double barrelled', 'Robert-John Smith-Jones-Wilson', 'R. Smith-Jones-Wilson'],
-  ])('%s initialiseName(%s, %s)', (_: string, a: string, expected: string) => {
-    expect(initialiseName(a)).toEqual(expected)
-  })
-})
-
 describe('findDefaultAddressInAddressList', () => {
   const baseAddress = {
     id: 1,
