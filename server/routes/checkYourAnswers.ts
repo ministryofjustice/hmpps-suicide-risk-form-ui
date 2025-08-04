2f30332d859f23a0d43a1f5c42e47ec2ef0ade61
karlfosterBCL <39554570+karlfosterBCL@users.noreply.github.com>
2025-08-04 14:15:28 +0100

    SRF-5 conditional rendering (#19)

--- server/routes/checkYourAnswers.ts
+++ server/routes/checkYourAnswers.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
 import AuditService, { Page } from '../services/auditService'
 
-import SuicideRiskApiClient from '../data/suicideRiskApiClient'
+import SuicideRiskApiClient, { SuicideRisk } from '../data/suicideRiskApiClient'
 import CommonUtils from '../services/commonUtils'
 
 export default function checkYourAnswersRoutes(
@@ -16,14 +16,18 @@ export default function checkYourAnswersRoutes(
   router.get('/check-your-answers/:id', async (req, res, next) => {
     await auditService.logPageView(Page.CHECK_YOUR_ANSWERS, { who: res.locals.user.username, correlationId: req.id })
 
+    const suicideRiskId: string = req.params.id
+    let suicideRisk: SuicideRisk = null
+
     const suicideRiskApiClient = new SuicideRiskApiClient(authenticationClient)
-    const suicideRisk = await suicideRiskApiClient.getSuicideRiskById(req.params.id as string, res.locals.user.username)
+    suicideRisk = await suicideRiskApiClient.getSuicideRiskById(suicideRiskId, res.locals.user.username)
 
     if (await commonUtils.redirectRequired(suicideRisk, res)) return
 
     res.render('pages/check-your-answers', {
       suicideRisk,
       currentPage,
+      suicideRiskId,
     })
   })
 
