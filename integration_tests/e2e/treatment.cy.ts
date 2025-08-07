a02b75e9c6ff61a40d4328077dab08a55e65fb2e
peter <peter.wilson@unilink.com>
2025-08-07 12:06:52 +0100

    SRF-3 added tests for all pages. Implementation already committed in previous PR

---
+++ integration_tests/e2e/treatment.cy.ts
@@ -0,0 +1,6 @@
+context('Treatment Page', () => {
+  it('navigates to report completed page if completed date set', () => {
+    cy.visit('/treatment/30000000-0000-0000-0000-33333333333')
+    cy.url().should('include', '/report-completed/30000000-0000-0000-0000-3333333333')
+  })
+})
