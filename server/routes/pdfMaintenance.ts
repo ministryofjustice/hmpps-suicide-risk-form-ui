7b2e4bfa47ebd2d69148298336f5e2f11099ad48
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2025-10-09 16:46:10 +0100

    SRF-84 (#64)
    * SRF-84
    - Remove unwanted redirect when accessing PDF

--- server/routes/pdfMaintenance.ts
+++ server/routes/pdfMaintenance.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
 import AuditService, { Page } from '../services/auditService'
-import CommonUtils from '../services/commonUtils'
 import SuicideRiskApiClient from '../data/suicideRiskApiClient'
 import { ErrorMessages } from '../data/uiModels'
 import { handleIntegrationErrors } from '../utils/utils'
@@ -10,17 +9,14 @@ export default function pdfMaintenanceRoutes(
   router: Router,
   auditService: AuditService,
   authenticationClient: AuthenticationClient,
-  commonUtils: CommonUtils,
 ): Router {
-  router.get('/pdf/:id', async (req, res, next) => {
+  router.get('/pdf/:id', async (req, res) => {
     await auditService.logPageView(Page.VIEW_PDF, { who: res.locals.user.username, correlationId: req.id })
 
     const suicideRiskId: string = req.params.id
 
     const suicideRiskApiClient = new SuicideRiskApiClient(authenticationClient)
     const suicideRisk = await suicideRiskApiClient.getSuicideRiskById(req.params.id as string, res.locals.user.username)
-
-    if (await commonUtils.redirectRequired(suicideRisk, suicideRiskId, res, authenticationClient)) return
 
     try {
       const stream: ArrayBuffer = await suicideRiskApiClient.getPdfById(suicideRiskId, res.locals.user.username)
