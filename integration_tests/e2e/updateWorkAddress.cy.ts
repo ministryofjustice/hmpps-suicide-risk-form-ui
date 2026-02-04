f9b3867ce5bfee0931e84b41d4c1bdb45712f91d
Joseph.Dundon <jdundon@unilink.com>
2026-02-04 16:10:17 +0000

    SRF-158 SRF Sign and Send - Adding address with field exceeding character limit returns blank page - Add appropriate error messages for length validation + tests

--- integration_tests/e2e/updateWorkAddress.cy.ts
+++ integration_tests/e2e/updateWorkAddress.cy.ts
@@ -74,4 +74,44 @@ context('Update Work Address page', () => {
       .should('exist')
       .should('contain.text', 'Postcode : This is a required value, please enter a value')
   })
+
+  function fillValidForm() {
+    cy.get('#description').invoke('val', 'Office').trigger('input')
+    cy.get('#buildingName').invoke('val', 'Building').trigger('input')
+    cy.get('#houseNumber').invoke('val', '1').trigger('input')
+    cy.get('#streetName').invoke('val', 'Street').trigger('input')
+    cy.get('#district').invoke('val', 'District').trigger('input')
+    cy.get('#townCity').invoke('val', 'Town').trigger('input')
+    cy.get('#county').invoke('val', 'County').trigger('input')
+    cy.get('#postcode').invoke('val', 'AA1 1AA').trigger('input')
+  }
+
+  function clickSaveButton() {
+    cy.get('#save-button').click()
+  }
+
+  const lengthTests = [
+    { selector: '#description', label: 'Office Description', max: 50 },
+    { selector: '#buildingName', label: 'Building Name', max: 35 },
+    { selector: '#houseNumber', label: 'Address Number', max: 35 },
+    { selector: '#streetName', label: 'Street Name', max: 35 },
+    { selector: '#district', label: 'District', max: 35 },
+    { selector: '#townCity', label: 'Town or City', max: 35 },
+    { selector: '#county', label: 'County', max: 35 },
+    { selector: '#postcode', label: 'Postcode', max: 8 },
+  ]
+
+  const suicideRiskId = '00000000-0000-0000-0000-600000000005'
+
+  lengthTests.forEach(({ selector, label, max }) => {
+    it(`validates ${label} exceeds ${max} characters`, () => {
+      cy.visit(`/update-work-address/${suicideRiskId}`)
+      fillValidForm()
+      cy.get(selector)
+        .invoke('val', 'X'.repeat(max + 1))
+        .trigger('input')
+      clickSaveButton()
+      cy.contains(`Please enter ${max} characters or less for ${label}`).should('be.visible')
+    })
+  })
 })
