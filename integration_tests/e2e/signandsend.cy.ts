e6c519faa024f2e4821995910346d5d6e0926f62
peter wilson <pwilson@unilink.com>
2026-03-17 12:33:55 +0000

    Fixed Failing Tests

--- integration_tests/e2e/signandsend.cy.ts
+++ integration_tests/e2e/signandsend.cy.ts
@@ -16,16 +16,13 @@ context('Sign and Send Page', () => {
   })
 
   it('shows only alternate address dropdown when no default address exists', () => {
-    cy.request('DELETE', `${wiremock}/mappings/11111111-1111-1111-1111-111111111111`)
-
     cy.visit('/sign-and-send/00000000-0000-0000-0000-600000000013')
-
+    cy.get('input[type="radio"][value="RO"]').check()
     cy.get('#alternate-address-text')
       .should('exist')
       .and('contain.text', 'Please specify the Work Location and Address that the Person on Probation should contact.')
     cy.get('#alternate-address-dropdown').should('exist')
     cy.get('#alternate-address').should('exist')
-
     cy.get('#workAddress').should('not.exist')
     cy.get('#update-address-button').should('not.exist')
     cy.get('#add-address-button').should('not.exist')
@@ -34,6 +31,7 @@ context('Sign and Send Page', () => {
 
   it('Continue saves and navigates away', () => {
     cy.visit('/sign-and-send/00000000-0000-0000-0000-600000000005')
+    cy.get('input[type="radio"][value="RO"]').check()
     cy.get('#continue-button').should('exist')
     cy.get('#continue-button').click()
     cy.url().should('include', '/check-your-answers/00000000-0000-0000-0000-600000000005')
